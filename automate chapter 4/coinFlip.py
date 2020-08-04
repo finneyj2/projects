d10ae816d6a40d617020fe36e29affd13800a45e
finneyj2 <jahliafinney@gmail.com>
2020-08-03 20:44:06 -0400

    changes were made

--- automate chapter 4/coinFlip.py
+++ automate chapter 4/coinFlip.py
@@ -1,37 +1,35 @@
 import random
-currentStreak = []
+tossedCoins = []
 
 
-def cointoss():
+def cointoss(tossedCoins):
     for experimentNumber in range(10000):
         cointoss = random.randint(0,1)
-        currentStreak.append(cointoss)
+        tossedCoins.append(cointoss)
         # Code that checks if there is a streak of 6 heads or tails in a row.
-    return currentStreak
+    return tossedCoins
 
 def run():
-    result = cointoss()
-    streak()
+    result = cointoss(tossedCoins)
+    streak(tossedCoins)
 
-def streak():
-    count = 0
+def streak(tossedCoins):
+    streak_Count = 0
     numberOfStreaks = 0
     for x in currentStreak:
-        if x == 0:
-            count+= 1
-            if x == 1:
-                count = 0
-            else count == 6:
+        if x == 0 and streak_Count < 6
+            streak_Count+= 1
+            if streak_Count == 6:
                 numberOfStreaks+= 1
 
-        else x == 1:
-            count+= 1
+        else:
+            streak_Count += 1
             if x == 0:
-                count = 0
-            else count == 6:
+                streak_Count = 0
+            elif streak_Count == 6:
                 numberOfStreaks+= 1
-        return numberOfStreaks
+    print('Chance of streak: %s%%' % (numberOfStreaks / 100))
+    return tossedCoins
 
 
 run()
-print('Chance of streak: %s%%' % (numberOfStreaks / 100))
