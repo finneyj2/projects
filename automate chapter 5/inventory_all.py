c28f05727086812aa16827d687d4915f323d9609
finneyj2 <jahliafinney@gmail.com>
2020-08-18 22:31:05 -0400

    almost there for the first part

--- automate chapter 5/inventory_all.py
+++ automate chapter 5/inventory_all.py
@@ -3,6 +3,10 @@ stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
 def display_inventory(inventory):
     print("Inventory:")
     item_total = 0
-    for k, v in inventory.items():
-
+    for k, v in stuff.items():
+        numbers = int(stuff.get(k))
+        item_total += numbers
+    print(str(stuff))
     print("Total number of items: " + str(item_total))
+
+display_inventory(stuff)
