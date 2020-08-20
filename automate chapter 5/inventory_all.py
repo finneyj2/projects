7010bace2397d229fda72212bfc4f07928ba1d9e
finneyj2 <jahliafinney@gmail.com>
2020-08-19 23:12:06 -0400

    made display_inventory display dictionary put in parameter

--- automate chapter 5/inventory_all.py
+++ automate chapter 5/inventory_all.py
@@ -2,20 +2,22 @@ import pprint
 
 stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
 
-def display_inventory(inventory):
+def display_Inventory(inventory):
     print("Inventory:")
     item_total = 0
-    for k, v in stuff.items():
-        numbers = int(stuff.get(k))
+    for k, v in inventory.items():
+        numbers = int(inventory.get(k))
         item_total += numbers
-        print(str(k) + ' ' + str(stuff.get(k)))
+        print(str(k) + ' ' + str(inventory.get(k)))
     print("Total number of items: " + str(item_total))
 
 def addToInventory(inventory, addedItems):
-    # your code goes here
+    for k, v in inv.items():
+        if inv[k] in dragonLoot:
+            print(test)
+
 
 inv = {'gold coin': 42, 'rope': 1}
 dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
 inv = addToInventory(inv, dragonLoot)
-displayInventory(inv)
-    
+display_Inventory(inv)
