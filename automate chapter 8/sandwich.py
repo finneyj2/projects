cca8103267f655f524c0207e06d0b76a23395338
finneyj2 <jahliafinney@gmail.com>
2020-08-31 20:17:37 -0400

    put all of code into a while loop and added another yes/no menu

--- automate chapter 8/sandwich.py
+++ automate chapter 8/sandwich.py
@@ -1,15 +1,17 @@
 import pyinputplus as pyip
 
 
-print('What is your preferred sandwich bread?')
-response = pyip.inputMenu(['Wheat', 'Sourdough', 'White'])
+responseY = pyip.inputYesNo("Would you like a sandwich?")
+while responseY == 'Yes':
+    print('What is your preferred sandwich bread?')
+    response = pyip.inputMenu(['Wheat', 'Sourdough', 'White'])
 
-decision = pyip.inputYesNo('Do you want protein?')
-if decision == 'yes':
-    pyip.inputMenu(['Ham', 'Chicken', 'Tuna', 'Bacon'])
-else:
-    print("Okay!")
+    decision = pyip.inputYesNo('Do you want protein?')
+    if decision == 'yes':
+        pyip.inputMenu(['Ham', 'Chicken', 'Tuna', 'Bacon'])
+    else:
+        print("Okay!")
 
-decision2 = pyip.inputYesNo('Do you want cheese?')
-if decision2 == 'yes':
-    pyip.inputMenu(['Swiss', 'Cheddar', 'Mozarella'])
+    decision2 = pyip.inputYesNo('Do you want cheese?')
+    if decision2 == 'yes':
+        pyip.inputMenu(['Swiss', 'Cheddar', 'Mozarella'])
