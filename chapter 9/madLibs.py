fd73ff715a55f90760f7083051902fd5dcbe8000
finneyj2 <jahliafinney@gmail.com>
2020-09-07 02:25:14 -0400

    beginnings of conditional logic, sucessfully able to find words in file

--- chapter 9/madLibs.py
+++ chapter 9/madLibs.py
@@ -1,7 +1,20 @@
+from pathlib import Path
 from PyDictionary import PyDictionary
 dict = PyDictionary()
 
-madL = open('libs.txt')
-content = madL.read()
-madL.close()
-print(content)
+def literature(file):
+    madL = open(file)
+    content = madL.read()
+    madL.close()
+    with open(file) as f:
+        if 'adjective' in f.read():
+            print('found')
+            p = Path('new_Libs.txt')
+            p.write_text(content)
+
+
+
+#def options():
+
+
+literature('libs.txt')
