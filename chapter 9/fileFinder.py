0efaa63a908615b5479a2d3698e461c3f28a65b1
finneyj2 <jahliafinney@gmail.com>
2020-09-15 21:47:14 -0400

    fixed textline, also program currently has no conditional statements so it's only known that it runs, not that it runs properly

--- chapter 9/fileFinder.py
+++ chapter 9/fileFinder.py
@@ -5,7 +5,7 @@ def search():
     p = Path('C:/Users/jahli/Practice/chapter 9')
     txtList = list(p.glob('*.txt'))
     txtlistStr = str(txtList)
-    ask = pyip.inputStr("What word are you looking for?")
+    ask = pyip.inputStr("What file are you looking for?")
     for x in txtList:
         x.read_text()
         y = re.search(ask, txtlistStr)
