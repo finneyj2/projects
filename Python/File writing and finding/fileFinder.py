061c7388b12a5f5bed75b46f298d42754c2eecc0
finneyj2 <jahliafinney@gmail.com>
2020-10-05 23:03:08 -0400

    fixed directory and regex issues

--- Python/File writing and finding/fileFinder.py
+++ Python/File writing and finding/fileFinder.py
@@ -1,18 +1,17 @@
 from pathlib import Path
-import pyinputplus as pyip, re
+import pyinputplus as pyip, re, glob
 
 def search():
-    p = Path('C:/Users/jahli/Practice/chapter 9')
-    txtList = list(p.glob('*.txt'))
-    txtlistStr = str(txtList)
-    print(txtlistStr)
+    p = Path('C:/Users/jahli/Projects/Python/File writing and finding')
+    txtList = list(glob.glob('*.txt'))
+    txtlistStr = str(list(glob.glob('*.txt')))
+    comp = re.compile(txtlistStr)
     ask = pyip.inputStr("What file are you looking for?")
-    for x in txtlistStr:
-        x.read_text()
-        print(x)
-        mo = re.search(ask, txtlistStr)
-        k = mo.group()
-        if  k != None:
+    print(txtlistStr)
+    for x in txtList:
+        mo = comp.match(ask)
+        print(mo)
+        if  mo is not None:
             print("Found")
         else:
             "Not found."
