5fb6276debdbcda0f2a026828778d5f62118e48c
finneyj2 <jahliafinney@gmail.com>
2020-10-26 16:47:11 -0400

    fixed strip and some other things

--- Python/word_searches/digrams.py
+++ Python/word_searches/digrams.py
@@ -1,23 +1,31 @@
-import Collections
+from collections import Counter
 
 """A Program to find digrams(letter pairings to occur consecutive)"""
 def main():
     #main function that will trigger the program
-    dict("3esl.txt")
+    dir = dict("3esl.txt")
+    for word in dir:
+        x = digram_and_freq_finder(dir)
+        print(x)
+
+
 def dict(dictionary):
-    with open(file) as in_file:
+    with open(dictionary) as in_file:
         loaded_txt = in_file.read().strip().split('\n')
         str_load = " "
         for x in loaded_txt:
-            str_load += str(x).rstripe("\n")+ " "
+            str_load += str(x).rstrip("\n")+ " "
         return str_load
 
 
 
-def digram_and_freq_finder():
+def digram_and_freq_finder(dictionary):
     di = ["tm", "mv", "vo", "or", "rd", "dl", "le"]
-    D_count = Counter()
+    D_count = Counter(di)
+    return D_count
 
+
+main()
 
 
     #digram and frequency finder
