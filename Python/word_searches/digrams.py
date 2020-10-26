cf607eb931e65ec44391af1957039231f8715dc2
finneyj2 <jahliafinney@gmail.com>
2020-10-26 18:32:25 -0400

    problems with counter

--- Python/word_searches/digrams.py
+++ Python/word_searches/digrams.py
@@ -1,12 +1,13 @@
 from collections import Counter
+import re
 
 """A Program to find digrams(letter pairings to occur consecutive)"""
 def main():
     #main function that will trigger the program
-    dir = dict("3esl.txt")
+    dir = dict("hm.txt")
     for word in dir:
         x = digram_and_freq_finder(dir)
-        print(x)
+    print(x)
 
 
 def dict(dictionary):
@@ -21,7 +22,9 @@ def dict(dictionary):
 
 def digram_and_freq_finder(dictionary):
     di = ["tm", "mv", "vo", "or", "rd", "dl", "le"]
-    D_count = Counter(di)
+    for let in di:
+        words = re.findall(str(let), dictionary)
+        D_count = Counter(words)
     return D_count
 
 
