8b0f949a9b84d2caf6b3bcc74425b8f29a0a5cff
finneyj2 <jahliafinney@gmail.com>
2020-11-09 16:28:25 -0500

    overhaul of previous skeleton to create a new one, reorganized layout of branch

--- Python/Testing/count_syllables_tester.py
+++
@@ -1,4 +0,0 @@
-import count_syllables
-import pytest
-
-def test_count(counted_syllables, expected_count):
---
+++ Python/word_searches/syllable_counter_tester/count_syllables_tester.py
@@ -0,0 +1,10 @@
+import count_syllables
+
+def main():
+    print("How many words do you want to test?")
+
+def test_count(counted_syllables, dictionary):
+"""Test function that compares syllable count with a dictionary"""
+
+def load_dict(dict):
+    """loads dictionary for program"""
