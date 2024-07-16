46e78049b79d40c73a58839e8482fdf74aa484bf
finneyj2 <>
2024-07-15 23:13:45 -0400

    removed school stuff

--- college_degree_and_salary/Tables-and-Stats-master/src/dataCalc.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-numPractice = np.array([72,200.00, 75,500.00, 71,800.00, 62,400.00])
-numPM = np.mean(numPractice)
-numPME =np.median(numPractice)
-numPSTD = np.std(numPractice)
-print(numPM)
-print(numPSTD)
-print(numPME)
